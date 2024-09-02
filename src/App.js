28f7acab5c205a666cd50f7408ac2d9c62f17245
AyoMeyanbe <ayomeyanbe@gmail.com>
2024-09-01 23:30:20 -0400

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,30 @@
-import logo from './logo.svg';
-import './App.css';
+import { useEffect, useState } from "react";
+import "./App.css";
+import axios, { Axios } from "axios";
+
 
 function App() {
+  // USING FETCH FUNCTION
+  // fetch("https://catfact.ninja/fact")
+  // .then((res) => res.json())
+  // .then((data) => {console.log(data)});
+  
+  const [catFact, setCatFact] = useState("");
+
+  const fetchCatFact = () => {
+    axios.get("https://catfact.ninja/fact").then((res) => {
+    setCatFact(res.data.fact)
+  })
+  }
+  // USING AXIOS
+  useEffect(() => {
+    fetchCatFact();
+  }, []);
+  
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <button onClick={fetchCatFact}>Generate Cat Fact</button>
+      <p>{catFact}</p>
     </div>
   );
 }
